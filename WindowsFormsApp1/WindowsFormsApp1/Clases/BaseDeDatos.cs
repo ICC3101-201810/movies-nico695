4cd03eb9d27ba0ef2ac7dcddf119d89eeac96922
= <=>
2018-06-12 17:30:51 -0400

    2

---
+++ WindowsFormsApp1/WindowsFormsApp1/Clases/BaseDeDatos.cs
@@ -0,0 +1,201 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+
+namespace WindowsFormsApp1
+{
+    [Serializable()]
+    public class BaseDeDatos
+    {
+        // SERIALIZACION
+
+
+        public static void SerializarP(BaseDeDatos bdd)
+        {
+
+            FileStream _fs = new FileStream(@"..\..\docs\bdd.dat", FileMode.Create);
+            BinaryFormatter _formatter = new BinaryFormatter();
+            _formatter.Serialize(_fs, bdd);
+            _fs.Close();
+        }
+        public static BaseDeDatos DeserializarP(string pArchivo)
+        {
+            if (File.Exists(@"..\..\docs\" + pArchivo) == true)
+            {
+                FileStream _fs = new FileStream(@"..\..\docs\" + pArchivo, FileMode.Open);
+                BinaryFormatter _formatter = new BinaryFormatter();
+                BaseDeDatos bdd1 = _formatter.Deserialize(_fs) as BaseDeDatos;
+                _fs.Close();
+                return bdd1;
+            }
+            return new BaseDeDatos();
+
+
+        }
+
+
+
+        List<Pelicula> peliculas = new List<Pelicula>();
+        List<Actor> actores = new List<Actor>();
+        List<Director> directores = new List<Director>();
+        List<Productor> productores = new List<Productor>();
+        List<Estudio> estudios = new List<Estudio>();
+
+
+        public class Pelicula
+        {
+            private string nombre;
+            private string director;
+            private string fecha;
+            private string descripcion;
+            private int presupuesto;
+            private Estudio estudio;
+
+            public Pelicula(string nombre, string director, string fecha, string descripcion, int presupuesto, Estudio estudio)
+            {
+                this.nombre = nombre;
+                this.director = director;
+                this.fecha = fecha;
+                this.descripcion = descripcion;
+                this.presupuesto = presupuesto;
+                this.estudio = estudio;
+            }
+
+            public string Nombre { get => nombre; set => nombre = value; }
+            public string Director { get => director; set => director = value; }
+            public string Fecha { get => fecha; set => fecha = value; }
+            public string Descripcion { get => descripcion; set => descripcion = value; }
+            public int Presupuesto { get => presupuesto; set => presupuesto = value; }
+            public Estudio Estudio { get => estudio; set => estudio = value; }
+        }
+
+
+        public class Persona
+        {
+            private string nombre;
+            private string apellido;
+            private string fechanacimiento;
+            private string biografia;
+
+
+
+
+            public Persona(string nombre, string apellido, string fechanacimiento, string biografia)
+            {
+                this.Nombre = nombre;
+                this.Apellido = apellido;
+                this.Fechanacimiento = fechanacimiento;
+                this.Biografia = biografia;
+
+            }
+
+            public string Nombre { get => nombre; set => nombre = value; }
+            public string Biografia { get => biografia; set => biografia = value; }
+            public string Apellido { get => apellido; set => apellido = value; }
+            public string Fechanacimiento { get => fechanacimiento; set => fechanacimiento = value; }
+
+
+        }
+
+
+        public class Actor:Persona
+        {
+            private string tipo="actor";
+
+            public Actor(string actor, string nombre, string apellido, string fechanacimiento, string biografia, int tipo) : base(nombre,apellido,fechanacimiento,biografia)
+            {
+                this.tipo = actor;
+            }
+
+        }
+
+        public class Director : Persona
+        {
+            private string tipo = "director";
+
+            public Director(string actor, string nombre, string apellido, string fechanacimiento, string biografia, int tipo) : base(nombre, apellido, fechanacimiento, biografia)
+            {
+                this.tipo = actor;
+            }
+
+        }
+
+        public class Productor : Persona
+        {
+            private string tipo = "productor";
+
+            public Productor(string actor, string nombre, string apellido, string fechanacimiento, string biografia, int tipo) : base(nombre, apellido, fechanacimiento, biografia)
+            {
+                this.tipo = actor;
+            }
+
+        }
+        public class Estudio
+        {
+            private string nombre;
+            private string direccion;
+            private string fechaApertura;
+
+            public Estudio(string nombre, string direccion, string fechaApertura)
+            {
+                this.nombre = nombre;
+                this.direccion = direccion;
+                this.fechaApertura = fechaApertura;
+            }
+
+            public string Nombre { get => nombre; set => nombre = value; }
+            public string Direccion { get => direccion; set => direccion = value; }
+            public string FechaApertura { get => fechaApertura; set => fechaApertura = value; }
+        }
+
+        public class PeliculaActor
+        {
+            private Pelicula pelicula;
+            private Actor actor;
+
+            public PeliculaActor(Pelicula pelicula, Actor actor)
+            {
+                this.Pelicula = pelicula;
+                this.Actor = actor;
+            }
+
+            public Pelicula Pelicula { get => pelicula; set => pelicula = value; }
+            public Actor Actor { get => actor; set => actor = value; }
+        }
+
+
+        public class PeliculaProductor
+        {
+            private Pelicula pelicula;
+            private Productor productor;
+
+            public PeliculaProductor(Pelicula pelicula, Productor productor)
+            {
+                this.Pelicula = pelicula;
+                this.Productor = productor;
+            }
+
+            public Pelicula Pelicula { get => pelicula; set => pelicula = value; }
+            public Productor Productor { get => productor; set => productor = value; }
+        }
+
+
+
+
+
+
+
+
+
+        
+    }
+
+
+    
+
+
+}
---
+++ WindowsFormsApp1/WindowsFormsApp1/Forms/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(210, 183);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 31);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Aloha";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(222, 224);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "歡迎";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(157, 137);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(188, 46);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Welcome";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 50F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(78, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(362, 76);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Bienvenido";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(519, 311);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
